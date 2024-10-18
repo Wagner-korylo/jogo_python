359857f663bc540b8f582dbc94a62befaa871829
wagner korylo <wagnerkorylo@gmail.com>
2024-10-17 22:24:42 -0300

    Tabuleiro

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -5,7 +5,7 @@ import pygame #importa a bibliotecagit
 pygame.init() #inicialização do pygame
 pygame.font.init() #inicialização do pacote de fontes no pygame
 
-screen = pygame.display.set_mode((500, 500))#Essa função configura a janela onde o jogo será exibido
+screen = pygame.display.set_mode((600, 600))#Essa função configura a janela onde o jogo será exibido
 pygame.display.set_caption('jogo da Velha Wagner')# Essa função altera o titulo da janela que sera exibido no aplicativo
 clock = pygame.time.Clock() #Essa função é usado para gerenciar a velocidade da atualização da tela
  
@@ -31,22 +31,29 @@ while running:#usado para repetir um bloco do codigo quando uma condição é ve
         if event.type == pygame.MOUSEBUTTONDOWN:# é uma seleção condicional que detecta, quando um botão do mouse for precionado.
             print ('Clicou')#é um comando que exibe uma mensagem clicou no consoilo ou na saida padrao do programa.
             cor_fundo += 1# 
-        if (cor_fundo > 3): # é uma condiçaõ que verifica se o valar da veriavel cor fundo é >3           
+        if cor_fundo > 3: # é uma condiçaõ que verifica se o valar da veriavel cor fundo é >3           
             cor_fundo = 1#
+
+            #desenha tabuleiro
+    pygame.draw.line(screen, 'white',(200, 0),(200, 600),10)
+    pygame.draw.line(screen, 'white',(400, 0),(400, 600),10)
+    pygame.draw.line(screen, 'white',(0, 200),(600, 200),10)
+    pygame.draw.line(screen, 'white',(0, 400),(600, 400),10)
            
        
     if cor_fundo == 1:# condicional que execulta um codigo de bloco se a condição especificada for verdadeira neste caso ela verifica se cor fundo é ugual a 1.2
-         screen.fill('blue')# preencha toda a estrutura do jogo com a cor específica azul
-         screen.blit(personagem_x, (250,250))#para desenhar na superficie uma imagem ou um texto na tela com o x
+        # screen.fill('blue')# preencha toda a estrutura do jogo com a cor específica azul
+        screen.blit(personagem_x, (60,30))#para desenhar na superficie uma imagem ou um texto na tela com o x
     elif cor_fundo == 2:#verifique se a cor de fundo será a 2, para deternimar que açaõ a ser tomada, 
-         screen.fill('blue')# preenche com o azul
-         screen.blit(personagem_y, (250,250))#para desenhar na superficie uma imagem ou um texto na tela com o y
+        screen.blit(personagem_y, (60,30))# preenche com o azul
+        screen.blit(personagem_x, (260,30))#para desenhar na superficie uma imagem ou um texto na tela com o x
+    elif cor_fundo == 2:#verifique se a cor de fundo será a 2, para deternimar que açaõ a ser tomada, 
+        screen.blit(personagem_y, (460,30))# preenche com o azul
          
-    else:
-         screen.fill('purple') #preenche com a cor roxa
-    
-
-    # flip() the display to put your work on screen
+        
+         #else:
+      
+     # flip() the display to put your work on screen
     pygame.display.flip() # a area atualizar a tela do jogo com todas as alterações que foram feitas desde a ultima atualização
 
     clock.tick(60)  # limits FPS to 60, é um objeto utilizado para controlar opm tempo e a taxa atualização do jogo
