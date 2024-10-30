a0a6e745d809116ba3f7d895468f4d2e7fcce38f
wagner korylo <wagnerkorylo@gmail.com>
2024-10-29 22:24:29 -0300

    Mudança das cores e espessura das linhas do tabuleiro

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -14,58 +14,27 @@ running = True #(a variavel running é usado como condição para determinar se 
                #running e true o loop continua; quando o ruinng se torna falso
 
 #redefinir os caracteres 'X' e '0'
-personagem_x = fonte_quadrinhos.render ('X', True, 'red')#cria uma superficie de imagem apartir do texto que vc dseseja exibir da tela. X e 0
+personagem_x = fonte_quadrinhos.render ('X', True, 'orange')#cria uma superficie de imagem apartir do texto que vc dseseja exibir da tela. X e 0
 personagem_0 = fonte_quadrinhos.render ('0', True, 'red')
 
 jogador_atual = personagem_x #inicializa o jogo vom o x
 
 rodadas = 0  # essa vareavel é usada para um controle de cor de fundo da tela de um jogo, permitindo alternancia entre diferentes nucleos
-
+tabuleiro_desenhado = False
 coordenada_x = 0
 coordenada_y = 0
+def desenha_tabuleiro(espessura, cor):
 
+    
+      #desenha tabuleiro 
+            #                          origem    destino
+            #                          (X ,Y)    (X < Y)
+    pygame.draw.line(screen, 'blue',(200, 0),(200, 600), espessura)
+    pygame.draw.line(screen, 'blue',(400, 0),(400, 600), espessura)
+    pygame.draw.line(screen, 'blue',(0, 200),(600, 200), espessura)
+    pygame.draw.line(screen, 'blue',(0, 400),(600, 400), espessura)
 
-
-while running:#usado para repetir um bloco do codigo quando uma condição é verdadeira
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():#essa função retorna uma lista de  todos os eventos que ocorree desde a ultima vez que voce chamou essa funçaõ
-                                 #isso inclui envento com o clic de mouse teclas precionadas movimento de mouse e comando de fechamentos de janelas 
-        if event.type == pygame.QUIT:# detecta quando um usuário tenta fexsr uma janela do jogo permite que vc respoonde a esse evento enserrando o 
-                                 #loopng principal e fechando o jogo principal
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:# é uma seleção condicional que detecta, quando um botão do mouse for precionado.
-            print('Clicou')#é um comando que exibe uma mensagem clicou no consoilo ou na saida padrao do programa.
-            click_pos = pygame.mouse.get_pos() # a posição do mouse quando ouver evento do cli
-            print('eixo x:' , click_pos[0])
-            print('eixo y:' , click_pos [1])
-            coordenada_x = click_pos[0]
-            coordenada_y = click_pos[1]
-            rodadas = rodadas + 1
-            if rodadas >= 10:
-                screen.fill('black')
-                rodadas = 0
-                coordenada_x = 0
-                coordenada_y = 0
-
-
-            if rodadas != 1:
-
-                if jogador_atual == personagem_x :
-                   jogador_atual = personagem_0  
-                else:
-                   jogador_atual = personagem_x   
-            else:
-                jogador_atual = personagem_x      
-
-            #desenha tabuleiro 
-    pygame.draw.line(screen, 'white',(200, 0),(200, 600),10)
-    pygame.draw.line(screen, 'white',(400, 0),(400, 600),10)
-    pygame.draw.line(screen, 'white',(0, 200),(600, 200),10)
-    pygame.draw.line(screen, 'white',(0, 400),(600, 400),10)
-           
-     #primeira linha    
-     #            x   y
+def faz_jogada():
     if  coordenada_x > 0 and coordenada_x < 200 and coordenada_y < 200:
         screen.blit(jogador_atual,(60,30))#  primeiro           preenche com o azul
     elif  coordenada_x >= 200 and coordenada_x < 400 and coordenada_y < 200:
@@ -87,10 +56,61 @@ while running:#usado para repetir um bloco do codigo quando uma condição é ve
     elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 400:
         screen.blit(jogador_atual, (260,430))# oitavo             para desenhar na superficie uma imagem ou um texto na tela com o x
     elif coordenada_x >= 400 and coordenada_y >= 400:
-        screen.blit(jogador_atual, (460,430))# nono               preenche com o azul
+        screen.blit(jogador_atual, (460,430))# nono   
+
+    
+
+
+           
+while running:#usado para repetir um bloco do codigo quando uma condição é verdadeira
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():#essa função retorna uma lista de  todos os eventos que ocorree desde a ultima vez que voce chamou essa funçaõ
+                                 #isso inclui envento com o clic de mouse teclas precionadas movimento de mouse e comando de fechamentos de janelas 
+        if event.type == pygame.QUIT:# detecta quando um usuário tenta fexsr uma janela do jogo permite que vc respoonde a esse evento enserrando o 
+                                 #loopng principal e fechando o jogo principal
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:# é uma seleção condicional que detecta, quando um botão do mouse for precionado.
+            print('Clicou')#é um comando que exibe uma mensagem clicou no consoilo ou na saida padrao do programa.
+            click_pos = pygame.mouse.get_pos() # a posição do mouse quando ouver evento do cli
+            print('eixo x:' , click_pos[0])
+            print('eixo y:' , click_pos [1])
+            coordenada_x = click_pos[0]
+            coordenada_y = click_pos[1]
+            rodadas = rodadas + 1
+
+            if rodadas >= 10:
+                screen.fill('black')
+                rodadas = 0
+                coordenada_x = 0
+                coordenada_y = 0
+                tabuleiro_desenhado = False
+            
+
+
+            if rodadas != 1:
+
+                if jogador_atual == personagem_x:
+                   jogador_atual = personagem_0 
+                else:
+                   jogador_atual = personagem_x   
+            else:
+                jogador_atual = personagem_x  
+
+            faz_jogada()
+
+    
+    if tabuleiro_desenhado == False:
+       desenha_tabuleiro(30, 'blue')
+       tabuleiro_desenhado = True
+ 
+           
+          
+     #primeira linha    
+     #            x   y
+    
 
    
-      
      # flip() the display to put your work on screen
     pygame.display.flip() # a area atualizar a tela do jogo com todas as alterações que foram feitas desde a ultima atualização
 
